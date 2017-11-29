b0ba9dcdc29fce66dfb2c2426e6e87c2a9a0f7e3
Cody Griffith <cody.griffith94@gmail.com>
2017-11-28 21:46:19 -0800

    Fix tests

--- tests/testthat/test_argument.R
+++ tests/testthat/test_argument.R
@@ -1,6 +1,13 @@
 context("Complex argument on numerics")
 
 test_that("At least numeric values work.", {
-	expect_identical(argument(numeric(1),numeric(0)), numeric(0))
+	expect_identical(argument(1,0), 0)
 	expect_identical(argument(0,1), pi/2)
+})
+
+
+test_that("Incorrect values.", {
+	expect_false(argument(1,1)==0)
+	expect_false(argument(1)==pi/2)
+	expect_error(argument())
 })
--- tests/testthat/test_modulus.R
+++ tests/testthat/test_modulus.R
@@ -3,5 +3,10 @@ context("Complex modulus on numerics")
 test_that("At least numeric values work.", {
 	expect_identical(modulus(numeric(0),numeric(0)), numeric(0))
 	expect_identical(modulus(3,4), 5)
-	expect_failure(modulus(1,1)==10)
+})
+
+test_that("Incorrect values.", {
+	expect_false(modulus(1,1)==2)
+	expect_false(modulus(1)==2)
+	expect_error(modulus(1,1,1))
 })
